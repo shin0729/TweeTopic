40b8eba518147a49281c05926151ff3d15245613
shin0729 <shin.yamaguchi0801@gmail.com>
2024-04-22 16:01:02 +0900

    feat: GensimのLDAモデルによりトピックモデリングを行う機能を追加しました．

--- src/sample.py
+++ src/sample.py
@@ -3,6 +3,9 @@ import os
 import re
 import torch
 from transformers import BertJapaneseTokenizer, BertForMaskedLM
+
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 def load_tweet():
     path = "/home/syamaguchi/twitter-data/sub/shoiti121109"
     files = os.listdir(path)
@@ -27,10 +30,10 @@ print(len(tweet))
 
 # Load pre-trained tokenizer
 tokenizer = BertJapaneseTokenizer.from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking')
-token =[]
+docs =[]
 for item in tweet:
-    token.append(tokenizer.tokenize(item))
-print(type(token))
+    docs.append(tokenizer.tokenize(item))
+print(type(docs))
 
 # トークンの前処理
 # 記号の削除
@@ -39,9 +42,68 @@ code_regex = re.compile('[\t\s!"#$%&\'\\\\()*+,-./:;；：<=>?@[\\]^_`{|}~○｢
 # 数字の削除
 num_regex = re.compile('\d+,?\d*')
 
-for i in token:
+for i in docs:
     for item in i:
         item = code_regex.sub("", item)
         item = num_regex.sub("0", item)
+print(docs)
+
+from gensim.models import Phrases
+bigram = Phrases(docs, min_count=20)
+for idx in range(len(docs)):
+    for token in bigram[docs[idx]]:
+        if '_' in token:
+            # Token is a bigram, add to document.
+            docs[idx].append(token)
 print(token)
 
+
+# Remove rare and common tokens.
+from gensim.corpora import Dictionary
+
+# Create a dictionary representation of the documents.
+dictionary = Dictionary(docs)
+
+# Filter out words that occur less than 20 documents, or more than 50% of the documents.
+dictionary.filter_extremes(no_below=20, no_above=0.5)
+
+# Bag-of-words representation of the documents.
+corpus = [dictionary.doc2bow(doc) for doc in docs]
+
+print('Number of unique tokens: %d' % len(dictionary))
+print('Number of documents: %d' % len(corpus))
+
+# Train LDA model.
+from gensim.models import LdaModel
+
+# Set training parameters.
+num_topics = 10
+chunksize = 2000
+passes = 20
+iterations = 400
+eval_every = None  # Don't evaluate model perplexity, takes too much time.
+
+# Make an index to word dictionary.
+temp = dictionary[0]  # This is only to "load" the dictionary.
+id2word = dictionary.id2token
+
+model = LdaModel(
+    corpus=corpus,
+    id2word=id2word,
+    chunksize=chunksize,
+    alpha='auto',
+    eta='auto',
+    iterations=iterations,
+    num_topics=num_topics,
+    passes=passes,
+    eval_every=eval_every
+)
+
+top_topics = model.top_topics(corpus)
+
+# Average topic coherence is the sum of topic coherences of all topics, divided by the number of topics.
+avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
+print('Average topic coherence: %.4f.' % avg_topic_coherence)
+
+from pprint import pprint
+pprint(top_topics)
