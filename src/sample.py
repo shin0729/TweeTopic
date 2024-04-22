361cd73c5d04322d1560b1b9c8e6b704c7c11db3
shin0729 <shin.yamaguchi0801@gmail.com>
2024-04-22 11:37:04 +0900

    feat: 前処理機能を追加しました

--- src/sample.py
+++ src/sample.py
@@ -1,5 +1,8 @@
 import json
 import os
+import re
+import torch
+from transformers import BertJapaneseTokenizer, BertForMaskedLM
 def load_tweet():
     path = "/home/syamaguchi/twitter-data/sub/shoiti121109"
     files = os.listdir(path)
@@ -19,4 +22,26 @@ def load_tweet():
                 contents.append(d)
     return contents
 tweet = load_tweet()
-print(tweet)
+print(len(tweet))
+
+
+# Load pre-trained tokenizer
+tokenizer = BertJapaneseTokenizer.from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking')
+token =[]
+for item in tweet:
+    token.append(tokenizer.tokenize(item))
+print(type(token))
+
+# トークンの前処理
+# 記号の削除
+code_regex = re.compile('[\t\s!"#$%&\'\\\\()*+,-./:;；：<=>?@[\\]^_`{|}~○｢｣「」〔〕“”〈〉'\
+    '『』【】＆＊（）＄＃＠？！｀＋￥¥％♪…◇→←↓↑｡･ω･｡ﾟ´∀｀ΣДｘ⑥◎©︎♡★☆▽※ゞノ〆εσ＞＜┌┘]')
+# 数字の削除
+num_regex = re.compile('\d+,?\d*')
+
+for i in token:
+    for item in i:
+        item = code_regex.sub("", item)
+        item = num_regex.sub("0", item)
+print(token)
+
